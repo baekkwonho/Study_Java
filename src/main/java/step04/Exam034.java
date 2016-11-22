095fa0805dc72121c3e62806a694b563fc867f4d
baekkwonho <kwonhobaek@gmail.com>
2016-11-22 18:26:54 +0900

    update step04

---
+++ src/main/java/step04/Exam034.java
@@ -0,0 +1,12 @@
+//주제: 연산자 - 산술 연산자
+package step04;
+
+public class Exam034 {
+  public static void main(String[] args) {
+    System.out.printf("10 + 8 = %d\n", 10 + 8);
+    System.out.printf("10 - 8 = %d\n", 10 - 8);
+    System.out.printf("10 * 8 = %d\n", 10 * 8);
+    System.out.printf("10 / 8 = %d\n", 10 / 8);
+    System.out.printf("10 %% 8 = %d\n", 10 % 8);
+  }
+}
---
+++ src/main/java/step04/Exam045.java
@@ -0,0 +1,16 @@
+//주제: 연산자 - 복합 대입(배정,할당) 연산자 사용법
+package step04;
+
+public class Exam045 {
+  public static void main(String[] args) {
+    int a = 10;
+    a = a + 20;
+    System.out.println(a);
+
+    a = 10;
+    a += 20; // +=, -=, *=, /=, %=
+    System.out.println(a);
+
+  }
+}
+
